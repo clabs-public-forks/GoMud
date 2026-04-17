c90e26bff480475d7f88f305d54353c090528f9b
Sean Casey <4674433+CaseyLabs@users.noreply.github.com>
2026-04-16 18:32:59 -0700

    Fix Windows release cross-build

---
+++ shutdown_signal_unix.go
@@ -0,0 +1,13 @@
+//go:build !windows
+
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func registerShutdownSignals(sigCh chan os.Signal) {
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM, syscall.SIGTSTP)
+}
